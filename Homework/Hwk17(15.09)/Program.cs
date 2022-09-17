824659a0780669cebd9e18cbdfa77021a5264cd2
Georgiy <goshalar@gmail.com>
2022-09-17 20:01:51 +0500

    Add Hwk 17 (Массив вещественных чисел + разница между макс и мин элементами массива)

---
+++ Homework/Hwk17(15.09)/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int size = 10;
+
+int[] array = new int[size]; 
+
+for (int i = 0; i < array.Length; i++) 
+
+{
+    array[i] = new Random().Next(0,10);
+    Console.Write(array[i] + " ");      
+}
+
+int max = array[0];
+int min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (max < array[i]) max = array[i];
+    if (min > array[i]) min = array[i];
+}
+
+int result = max - min;
+Console.WriteLine("\n" + $"Разница между максимальным и минимальным значением массива : {result}");
