6a28c6910ab9a806f10bd3f5ef9d60876b93dbd0
Georgiy <goshalar@gmail.com>
2022-09-17 18:26:05 +0500

    Add Hwk15 (Задача 34. Заполнение массива случайными трехзначными числами + поиск четных чисел в массиве)

---
+++ Homework/Hwk15(15.09)/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+
+Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+int[] array = new int[size];  
+
+for (int i = 0; i < array.Length; i++) 
+
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write(array[i] + " ");   
+    if (array[i] % 2 == 0) count +=1;
+
+}
+
+Console.WriteLine();
+if (count > 0) Console.WriteLine($"Количество четных чисел в массиве: {count}");
+else Console.WriteLine($"Четных чисел в массиве нет");
