22c34f26008a1406caf3e6e923450460e6c2d0b2
Georgiy <goshalar@gmail.com>
2022-09-24 17:10:31 +0500

    add hwk 23 (Задача 54. Задайть двумерный массив + упорядочить элементы в строках)

---
+++ Homework/Hwk23(24.09)/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns);
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(21);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+void UpdateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int e = 0; e < array.GetLength(0) - 1; e++)
+      {
+        if (array[i, e] < array[i, e + 1])
+        {
+          int temp = array[i, e + 1];
+          array[i, e + 1] = array[i, e];
+          array[i, e] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+UpdateArray(array);
+PrintArray(array);
+
+
+
+
