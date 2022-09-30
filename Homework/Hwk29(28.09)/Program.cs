a5d60159ec82a9e828eee14d440e241e55906ef4
Georgiy <goshalar@gmail.com>
2022-09-30 13:31:29 +0500

    Add Hwk 29 (Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N)

---
+++ Homework/Hwk29(28.09)/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumRec(int m, int n)
+{
+    int s = 0;
+    for (int i = m; i <= n; i++)
+    {
+        if (i % 1 == 0)
+        s=s+i; 
+    }
+    return s;  
+}
+
+System.Console.WriteLine(SumRec(m,n));
